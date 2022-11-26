fbf75e2b89618f819fb27729c7c8288a792b3b61
senkevich <lofylofy780@gmail.com>
2022-11-26 17:34:45 +0300

    customize static and image

--- backend/blog/settings/base.py
+++ backend/blog/settings/base.py
@@ -123,8 +123,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_intermediate')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
 # Media
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
